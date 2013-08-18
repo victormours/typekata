f60eb93b971789de84dedd411a47ac24c72e55fe
Victor Mours <victor.mours@gmail.com>
2013-08-18 17:06:57 -0500

    do not precompile assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Typekata::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -77,4 +77,6 @@ Typekata::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.debug = true
 end
