697beb9c1cac1dbc6b7041df1d4f260518edd082
Johan Girod <johangirod@gmail.com>
2013-09-06 18:31:53 +0200

    Deleting application file... This branch is JUST for the blog

--- app/models/kata.rb
+++
@@ -1,2 +0,0 @@
-class Kata < ActiveRecord::Base
-end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Typekata::Application.config.session_store :cookie_store, key: '_typekata_session'
