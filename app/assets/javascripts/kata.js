972021c0ff508100986f2acc6ca2870d58002942
Victor Mours <victor.mours@gmail.com>
2013-10-27 12:40:21 -0500

    Display wpm when doing a kata

---
+++ app/assets/javascripts/kata.js
@@ -0,0 +1,51 @@
+var TypeKata = TypeKata || {};
+
+TypeKata.Kata = (function(){
+  var startTime = null
+  var kataInput = "[data-bind=kata-input]"
+  var wpmDisplay = "[data-bind=wpm-display]"
+
+  function bindKataInput(){
+    $(kataInput).on("keyup", function(){
+      initTimer()
+      updateWPM()
+    })
+  }
+
+  function initTimer(){
+    if(startTime == null){
+      startTime = currentTime()
+    }
+  }
+
+  function updateWPM(){
+    wpm = computeWPM(characterCount(), startTime, currentTime())
+    $(wpmDisplay).html(wpm)
+  }
+
+  function currentTime(){
+    return new Date().getTime()
+  }
+
+  function characterCount(){
+   return $(kataInput)[0].value.length 
+  }
+
+  function computeWPM(characterCount, startTime, currentTime){
+    var words = characterCount / 5
+    var minutes = (currentTime - startTime)/ (60 * 1000)
+    return Math.round(words/minutes)
+  }
+
+  function init(){
+    bindKataInput()
+  }
+
+  return {
+    init: init
+  }
+})()
+
+$(function() {
+  TypeKata.Kata.init()
+})
