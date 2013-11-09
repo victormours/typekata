0570ff2ef8c4facdd8ca2781da5bf64c1f8cc6b3
Victor Mours <victor.mours@gmail.com>
2013-11-09 12:22:09 -0600

    Extract google analytics code to a partial

--- config/initializers/secrets.rb
+++ config/initializers/secrets.rb
@@ -1,13 +1,5 @@
 module Secrets
   def self.[](key)
-    unless @config
-      raw_config = File.read("#{Rails.root}/config/secrets.yml")
-      @config = YAML.load(raw_config)[Rails.env].symbolize_keys
-    end
-    @config[key]
-  end
-
-  def self.[]=(key, value)
-    @config[key.to_sym] = value
+    ENV[key]
   end
 end
